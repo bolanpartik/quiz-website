5167c83d03751c2b1357d8c62dfb5c522f321535
Partik Bolan <bolanpartik@gmail.com>
2024-08-22 00:54:18 +0530

    dynamically show question and options

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const apiKey = 'c8NVsuj4eYyKCxx9FEB4173BOqKNQzshH0NsoqOx';
 
 const url = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&difficulty=hard&limit=2`
 
-let apiResult=''
+let index = 0;
+let apiResult = ''
 const getQuestions = async () => {
     try {
         const res = await fetch(url, {
@@ -15,9 +16,36 @@ const getQuestions = async () => {
         })
         apiResult = await res.json()
         console.log(apiResult)
+        displayQuestion()
     } catch (e) {
         console.log("Error", e)
     }
+}
+
+const displayQuestion = () => {
+    questionContainer.textContent = '';
+    optionsContainer.innerHTML = ''
+
+    questionContainer.textContent = apiResult[index].question
+
+    submitButton.disabled = false;
+
+    Object.entries(apiResult[index].answers).forEach(([key, option]) => {
+        if (option !== null) {
+
+            const radioElement = document.createElement('input')
+            radioElement.type = 'radio'
+            radioElement.name = 'options'
+            radioElement.id = key
+
+            const radioLabel = document.createElement('label')
+            radioLabel.setAttribute('for', key)
+            radioLabel.textContent = option
+
+            optionsContainer.append(radioElement, radioLabel)
+            optionsContainer.append(document.createElement('br'))
+        }
+    })
 }
 
 document.addEventListener('DOMContentLoaded', getQuestions)
